3bf247b7d34f3f500292af76225a52ecf38579ef
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-05-11 11:24:44 +0000

    added proxy server

--- src/Constants.js
+++ src/Constants.js
@@ -2,9 +2,12 @@
 
 export const IMG_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
-export const restaurantApi = "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+export const swiggyApi = "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
 
 
-export const swiggyApi = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630"
+// export const swiggyApi = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630"
 
-export const SwiggyMenuApi = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId="
+// export const SwiggyMenuApi = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId="
+
+export const SwiggyMenuApi = "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=28.65200&lng=77.16630&restaurantId="
+
