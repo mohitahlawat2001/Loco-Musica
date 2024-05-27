e816d7fd43c1e6a76a77ad2a4a6a1b9ea856f142
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2024-05-27 17:18:01 +0000

    added footer with welcome msg and our values

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,22 @@ import { useContext } from "react";
 const Footer = () => {
         const { user } = useContext(UserContext);
         return (
-                <div className="bg-gray-900 text-white py-4">
+                <div className="bg-gray-900 text-white py-4 mt-4">
+                        <div className="flex flex-row justify-around" >
                         <div className="container mx-auto px-4">
-                                <h1 className="text-2xl">Footer</h1>
+                                <h1 className="text-2xl">Loco Musica: Food & Tunes in Harmony
+                        </h1>
                         </div>
                         <div className="container mx-auto px-4">
                                 <p className="text-center">Welcome {user.name}!</p>
                                 <p className="text-center">Email: {user.email}</p>
-                                </div>
+                        </div>
+                        </div>
+
+                        <div className="mt-6 pt-4 text-center">
+                                <p>© 2024 Loco Musica. All rights reserved.</p>
+                                <p>Bringing the rhythm to your taste buds, one order at a time!</p>
+                        </div>
                 </div>
         );
 };
