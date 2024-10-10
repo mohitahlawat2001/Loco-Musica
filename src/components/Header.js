3bd43dc20b130d851a95c96cf84406e8b854780f
Abhijeet Kumar <abhijeetkumar3245@gmail.com>
2024-10-10 11:47:56 +0530

    Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,8 +30,8 @@ const [isMenuOpen , setMenuOpen] = useState(false);
 
   return (
     <>
-    <div className="max-w-screen-lg mx-auto flex justify-between items-center border border-gray-300 rounded-lg shadow-md mt-4 py-1 px-4 flex-wrap sticky top-0 z-10 bg-white mix-blend-normal">
-  <div className="my-2 hidden lg:block">
+    <div className="max-w-screen-lg mx-auto flex justify-between items-center border border-gray-300 rounded-lg shadow-md mt-4 py-1 px-4 sticky top-0 z-10 bg-white mix-blend-normal">
+  <div className="my-2  hidden md:block">
     <Link to="/">
       <img
         src={Loco}
@@ -42,26 +42,28 @@ const [isMenuOpen , setMenuOpen] = useState(false);
     </Link>
   </div>
 
-  <div className="md:hidden flex items-center" onClick={toggleMenu}>
+  <div className="md:hidden flex items-center cursor-pointer" onClick={toggleMenu}>
     <FontAwesomeIcon icon={faBars} size="2xl" />
   </div>
 
   <div className={`w-full md:w-auto ${isMenuOpen ? "block" : "hidden"} md:block`}>
-    <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-5 p-2 m-2">
-    <li>
-  <Link className="relative inline-block hover:text-pink-300 font-bold transition-transform duration-300 transform hover:scale-105" to="/">
-    Home
-    <span className="absolute left-0 bottom-0 h-1 bg-pink-300 transform scale-x-0 transition-transform duration-300 origin-right hover:scale-x-100"></span>
-  </Link>
-</li>
+    <ul className="flex flex-col md:flex-row justify-center items-center space-y-2 md:space-y-0 md:space-x-5 p-2 m-2">
       <li>
-        <Link className="relative inline-block hover:text-pink-300 font-bold transition-transform duration-300 transform hover:scale-105" to="/about">About
-        <span className="absolute left-0 bottom-0 h-1 bg-pink-300 transform scale-x-0 transition-transform duration-300 origin-right hover:scale-x-100"></span>
+        <Link className="relative inline-block hover:text-pink-300 font-bold transition-transform duration-300 transform hover:scale-105" to="/">
+          Home
+          <span className="absolute left-0 bottom-0 h-1 bg-pink-300 transform scale-x-0 transition-transform duration-300 origin-right hover:scale-x-100"></span>
         </Link>
       </li>
       <li>
-        <Link className="relative inline-block hover:text-pink-300 font-bold transition-transform duration-300 transform hover:scale-105" to="/contact">Contact
-        <span className="absolute left-0 bottom-0 h-1 bg-pink-300 transform scale-x-0 transition-transform duration-300 origin-right hover:scale-x-100"></span>
+        <Link className="relative inline-block hover:text-pink-300 font-bold transition-transform duration-300 transform hover:scale-105" to="/about">
+          About
+          <span className="absolute left-0 bottom-0 h-1 bg-pink-300 transform scale-x-0 transition-transform duration-300 origin-right hover:scale-x-100"></span>
+        </Link>
+      </li>
+      <li>
+        <Link className="relative inline-block hover:text-pink-300 font-bold transition-transform duration-300 transform hover:scale-105" to="/contact">
+          Contact
+          <span className="absolute left-0 bottom-0 h-1 bg-pink-300 transform scale-x-0 transition-transform duration-300 origin-right hover:scale-x-100"></span>
         </Link>
       </li>
       <li>
@@ -81,7 +83,7 @@ const [isMenuOpen , setMenuOpen] = useState(false);
 
   <div className="p-2 m-2">
     {login ? (
-      <div onClick={handleLogout}>
+      <div onClick={handleLogout} className="cursor-pointer">
         <FontAwesomeIcon icon={faRightFromBracket} size="2xl" />
       </div>
     ) : (
@@ -93,11 +95,7 @@ const [isMenuOpen , setMenuOpen] = useState(false);
 </div>
 
 <h1 data-testid="online-status">
-  {isOnline ? (
-    <div className="border mx-auto bg-green-400 rounded-md h-2 w-1/2 md:w-1/3" />
-  ) : (
-    <div className="border mx-auto bg-red-400 rounded-md h-2 w-1/2 md:w-1/3" />
-  )}
+  <div className={`border mx-auto rounded-md h-2 w-1/2 md:w-1/3 ${isOnline ? 'bg-green-400' : 'bg-red-400'}`} />
 </h1>
 
     </>
